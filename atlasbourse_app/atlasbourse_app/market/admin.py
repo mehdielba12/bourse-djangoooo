71a5b9d8119a502608745b6531cca5a1db98cf9e
mehdielba12 <elbalghitimehdi2@gmail.com>
2025-12-02 17:29:02 +0100

    Delete atlasbourse_app directory

--- atlasbourse_app/atlasbourse_app/market/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Stock, Portfolio, Position, Transaction, CashOperation
-
-admin.site.register(Stock)
-admin.site.register(Portfolio)
-admin.site.register(Position)
-admin.site.register(Transaction)
-admin.site.register(CashOperation)
